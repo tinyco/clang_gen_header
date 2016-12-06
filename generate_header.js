49995822ea0252ab981b79d078cadea14c1330aa
tinyco <tiny.code.editor@gmail.com>
2016-12-06 13:14:33 +0900

    refactoring, use appendFileSync.

--- generate_header.js
+++ generate_header.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
-let error = i => i && console.log(JSON.stringify(i, null, 4))
-let init = filename => fs.writeFile (filename, '' , err => error(err))
-let write = (filename, str) => fs.appendFile(filename, str ? str + '\n' : '' , err => error(err)) + '\n'
+let init = filename => fs.writeFileSync (filename, '')
+let write = (filename, str) => fs.appendFileSync(filename, str + '\n')
 
 if (process.argv.length < 3) {
   console.log('Error: illegal argument.')
@@ -12,31 +11,29 @@ if (process.argv.length < 3) {
       if(err) {
         error(err)
       } else {
-        let str = "";
         let h_file = c_file.replace(/.c$/,'.gen.h')
+        init(h_file)
         const flag = c_file.toUpperCase().replace(/[\/\.]/g,'_')
-        str = str + '//This file is auto-generated from ' + c_file + '\n'
-        str = str + '#ifndef ' + flag + '\n'
-        str = str + '#define ' + flag + '\n'
+        write(h_file, '//This file is auto-generated from ' + c_file)
+        write(h_file, '#ifndef ' + flag)
+        write(h_file, '#define ' + flag)
 
-        str = str + '//EXPORT' + '\n'
+        write(h_file, '//EXPORT')
         sorce_code
           .split('/*')
           .map(i => i.split('*/')[0])
           .filter(i => i.indexOf('EXPORT') === 0)
           .map(i => i.replace('EXPORT\n',''))
-          .map(i => str = str + i + '\n')
+          .map(i => write(h_file, i))
 
-        str = str + '//PUBLIC' + '\n'
+        write(h_file, '//PUBLIC')
         sorce_code
           .split('\n')
           .filter(i => i.indexOf('//PUBLIC') !== -1)
           .map(i => i.replace('//PUBLIC',''))
-          .map(i => str = str + i + '\n')
+          .map(i => write(h_file, i))
 
-        str = str + '#endif' + '\n'
-        init(h_file)
-        write(h_file, str)
+        write(h_file, '#endif')
       }
     })
   })
